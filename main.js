37601b7af1c198529525d2a139ad9653f850acff
titopotito <jdf.tito@gmail.com>
2023-06-20 17:53:24 +0800

    Displayed list of expenses and total expense. Modified design.

--- main.js
+++ main.js
@@ -1,24 +1,55 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const expenses = [
+    {
+        id: 1,
+        title: "Rent",
+        amount: 1000,
+    },
+    {
+        id: 2,
+        title: "Groceries",
+        amount: 200,
+    },
+    {
+        id: 3,
+        title: "Utilities",
+        amount: 150,
+    },
+    {
+        id: 4,
+        title: "Transportation",
+        amount: 100,
+    },
+    {
+        id: 5,
+        title: "Entertainment",
+        amount: 50,
+    },
+];
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const expenseList = document.getElementById("expense-list");
+const totalExpense = document.getElementById("total-expense");
 
-setupCounter(document.querySelector('#counter'))
+// Format and display expenses from expense[] array
+expenses.forEach((expense) => {
+    let expenseEntry = document.createElement("li");
+    expenseEntry.innerHTML = `
+      <li>
+        <p>${expense.title}</p>
+        <p>PHP <span>${expense.amount.toLocaleString("en-US", {
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2,
+        })}</span></p>
+      </li>
+    `;
+    expenseList.appendChild(expenseEntry);
+});
+
+// Calculate total expense, format it with commas and decimals, then display it
+totalExpense.textContent = expenses
+    .reduce(function (total, expense) {
+        return total + expense.amount;
+    }, 0)
+    .toLocaleString("en-US", {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    });
