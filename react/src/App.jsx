a7dbcb04ec8fadee1e804cbe95c28bd51a3d4063
titopotito <jdf.tito@gmail.com>
2023-07-04 02:21:02 +0800

    Activity 4 (partial)

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,43 +1,63 @@
-import { useState } from "react";
+import { useState, useEffect, useMemo, useRef, forwardRef } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+// const sampleData = [
+//     {
+//         id: 1,
+//         title: "Rent",
+//         amount: 1000,
+//     },
+//     {
+//         id: 2,
+//         title: "Groceries",
+//         amount: 200,
+//     },
+//     {
+//         id: 3,
+//         title: "Utilities",
+//         amount: 150,
+//     },
+//     {
+//         id: 4,
+//         title: "Transportation",
+//         amount: 100,
+//     },
+//     {
+//         id: 5,
+//         title: "Entertainment",
+//         amount: 50,
+//     },
+// ];
+
 function App() {
-    const [expenses, setExpenses] = useState([
-        {
-            id: 1,
-            title: "Rent",
-            amount: 1000,
-        },
-        {
-            id: 2,
-            title: "Groceries",
-            amount: 200,
-        },
-        {
-            id: 3,
-            title: "Utilities",
-            amount: 150,
-        },
-        {
-            id: 4,
-            title: "Transportation",
-            amount: 100,
-        },
-        {
-            id: 5,
-            title: "Entertainment",
-            amount: 50,
-        },
-    ]);
+    const [expenses, setExpenses] = useState([]);
+    const titleRef = useRef();
+    const amountRef = useRef();
+
+    useEffect(() => {
+        expenses.length > 0 ? alert("Added New Expense") : console.log("No Expenses");
+    }, [expenses]);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const title = titleRef.current.value;
+        const amount = amountRef.current.value;
+
+        setExpenses([...expenses, { id: Math.random(), title: title, amount: +amount }]);
+
+        titleRef.current.value = "";
+        amountRef.current.value = "";
+    };
 
     return (
         <>
             <h1>James Tito's Expense Tracker</h1>
-            <form id="input-section">
-                <InputGroup id="description" label="Description" type="text" placeholder="Enter Description" />
-                <InputGroup id="amount" label="Amount" type="number" placeholder="Enter Amount" />
+            <form id="input-section" onSubmit={handleSubmit}>
+                <InputGroup ref={titleRef} label="Description" type="text" placeholder="Enter Description" />
+                <InputGroup ref={amountRef} label="Amount" type="number" placeholder="Enter Amount" />
                 <div>
                     <button type="submit">Add Expense</button>
                 </div>
@@ -47,20 +67,20 @@ function App() {
             </h2>
             <section id="display-section">
                 <h3>My Expenses</h3>
-                <List data={expenses} />
+                <ExpenseList data={expenses} />
             </section>
         </>
     );
 }
 
-function InputGroup(props) {
+const InputGroup = forwardRef((props, ref) => {
     return (
         <div className="form-group">
             <label>{props.label}</label>
-            <input type={props.type} placeholder={props.placeholder} id={props.id} />
+            <input type={props.type} placeholder={props.placeholder} ref={ref} required />
         </div>
     );
-}
+});
 
 function Total(props) {
     const total = props.data
@@ -79,21 +99,31 @@ function Total(props) {
     );
 }
 
-function List(props) {
-    const expenseList = props.data.map((expense) => (
-        <li key={expense.id.toString()} className="expense-li">
-            <p>{expense.title}</p>
-            <p className={expense.amount > 100 ? "red" : "green"}>
-                PHP{" "}
-                <span>
-                    {expense.amount.toLocaleString("en-US", {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                    })}
-                </span>
-            </p>
-        </li>
-    ));
+function ExpenseList(props) {
+    if (props.data.length === 0) {
+        return <p className="default-message">No Expense Written</p>;
+    }
+
+    const expenseList = useMemo(
+        () =>
+            props.data.map((expense) => (
+                <li key={expense.id.toString()} className="expense-li">
+                    <p>{expense.title}</p>
+                    <p className={expense.amount > 100 ? "red" : "green"}>
+                        PHP{" "}
+                        <span>
+                            {expense.amount.toLocaleString("en-US", {
+                                minimumFractionDigits: 2,
+                                maximumFractionDigits: 2,
+                            })}
+                        </span>
+                    </p>
+                </li>
+            )),
+        [props.data]
+    );
+
+    console.log(expenseList);
 
     return <ul>{expenseList}</ul>;
 }
